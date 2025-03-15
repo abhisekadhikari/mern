8adc9dd320039f1c807efa02e06da2652c4e7285
Abhisek Adhikari <abhisekadhikari1906@gmail.com>
2025-03-15 15:24:12 +0530

    feat: add like and comment functionality to posts; implement endpoints for liking, commenting, and deleting comments

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -99,9 +99,117 @@ const deletePost = asyncErrorHandler(async (req, res) => {
     })
 })
 
+/**
+ * @desc   Like a post
+ * @route  PUT /post/:id/like
+ * @access Private
+ */
+const likePost = asyncErrorHandler(async (req, res) => {
+    const { id } = req.params
+
+    const post = await Post.findById(id)
+
+    if (!post) throw new AppError(404, "Post not found", "Invalid post ID")
+
+    const alreadyLiked = post.likes.some(
+        (usr) => usr.user.toString() === req.user._id
+    )
+
+    if (alreadyLiked)
+        throw new AppError(400, "Bad Request", "Post already liked")
+
+    // Add the like
+    post.likes.push({ user: req.user._id })
+    await post.save()
+
+    res.status(200).json({
+        success: true,
+        message: "Post liked successfully",
+        data: post.likes,
+    })
+})
+
+/**
+ * @desc   Add a comment to a post
+ * @route  POST /post/:id/comment
+ * @access Private
+ */
+const commentPost = asyncErrorHandler(async (req, res) => {
+    const { id } = req.params
+    const { text } = req.body
+
+    // Find the post
+    const post = await Post.findById(id)
+    if (!post) throw new AppError(404, "Post not found", "Invalid post ID")
+
+    // Ensure the comment text is not empty
+    if (!text || text.trim().length === 0)
+        throw new AppError(400, "Bad Request", "Comment text cannot be empty")
+
+    // Create the comment object
+    const newComment = {
+        user: req.user._id,
+        text,
+        name: req.user.name,
+        avatar: req.user.avatar,
+        date: new Date(),
+    }
+
+    // Add the comment to the post
+    post.comments.push(newComment)
+    await post.save()
+
+    res.status(201).json({
+        success: true,
+        message: "Comment added successfully",
+        data: post.comments,
+    })
+})
+
+/**
+ * @desc   Delete a comment from a post
+ * @route  DELETE /post/:id/comment/:commentId
+ * @access Private
+ */
+const deleteComment = asyncErrorHandler(async (req, res) => {
+    const { id, commentId } = req.params
+
+    // Find the post
+    const post = await Post.findById(id)
+    if (!post) throw new AppError(404, "Post not found", "Invalid post ID")
+
+    // Find the comment
+    const commentIndex = post.comments.findIndex(
+        (comment) => comment._id.toString() === commentId
+    )
+    if (commentIndex === -1)
+        throw new AppError(404, "Comment not found", "Invalid comment ID")
+
+    // Check if the user is the owner of the comment
+    if (post.comments[commentIndex].user.toString() !== req.user._id)
+        throw new AppError(
+            403,
+            "Unauthorized",
+            "You are not allowed to delete this comment"
+        )
+
+    // Remove the comment
+    post.comments.splice(commentIndex, 1)
+    await post.save()
+
+    res.status(200).json({
+        success: true,
+        message: "Comment deleted successfully",
+        data: post.comments,
+    })
+})
+
 module.exports = {
     createPost,
     getAllPosts,
     updatePost,
     deletePost,
+    likePost,
+    commentPost,
+    deleteComment,
 }
